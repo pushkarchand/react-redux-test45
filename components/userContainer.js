b211d28685ac661a71b0c5dc64b73763a608f1e4
pushkarchand <pushkarbc.11ec@gmail.com>
2020-05-04 13:00:21 +0530

    1 file modified

--- components/userContainer.js
+++ components/userContainer.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 import {useSelector,useDispatch} from 'react-redux';
 import {fetchUserRequest,fetchUserSuccess,fetchUserFailure,fetchUser} from '../redux'
 function UserContainer () {
@@ -6,14 +6,35 @@ function UserContainer () {
   const isLoading=useSelector(state=>state.user.isLoading);
   const error=useSelector(state=>state.user.error);
   const dispatch=useDispatch();
+  const [searchName,setSearchName]=useState('');
+  const [filteredUsers,setFilteredUsers]=useState([]);
   useEffect(()=>{
     dispatch(fetchUser())
   },[])
-  return isLoading ?(<h2>Loading</h2>
-    ):error?(<h2>{error}</h2>)
-    :(users && users.length && users.map(user=>{
-    return <p id={user.id}>{user.name}</p>
-  }))
+
+  useEffect(()=>{
+    console.log(searchName);
+     if(searchName!==''){
+      const userList=users.filter(user=>{
+        return user.name.indexOf(searchName)!==-1;
+        })
+       setFilteredUsers(userList);
+     } else {
+       setFilteredUsers(users);
+     }
+  },[searchName,users])
+  return (
+    <React.Fragment>
+    <input type="text" placeholde="Search by name" value={searchName} 
+    onChange={(event)=>setSearchName(event.target.value)}/>
+        {
+          isLoading ?(<h2>Loading</h2>
+            ):error?(<h2>{error}</h2>)
+            :(filteredUsers && filteredUsers.length && filteredUsers.map(user=>{
+            return <p id={user.id}>{user.name}</p>
+          }))}
+  </React.Fragment>
+  )
 }
 
 
